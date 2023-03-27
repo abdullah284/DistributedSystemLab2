188c1812415a192b19189e8f0059a57c4bc4bfd7
abdullah284 <72227604+abdullah284@users.noreply.github.com>
2023-03-27 17:56:04 +0100

    Update GRPCClientService.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -47,7 +47,7 @@ public class GRPCClientService {
 }
 
 
-public String multiply(){
+     public String multiply(){
 		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
 		.usePlaintext()
 		.build();
